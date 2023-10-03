4c304ee1b4ee1a6a22d29d833405a7b85584c929
1clogon1 <94410404+1clogon1@users.noreply.github.com>
2023-10-03 15:00:15 +0300

    Update Api.kt

--- app/src/main/java/com/example/airnet/Api/Api.kt
+++ app/src/main/java/com/example/airnet/Api/Api.kt
@@ -9,15 +9,12 @@ import retrofit2.http.Query
 
 
 interface Api {
-//
-//    @GET("allStreets")//https://dummyjson.com
-//    suspend fun getStreet(): Response<Street>
 
-    @GET("allStreets")//https://dummyjson.com
+    @GET("allStreets")
     suspend fun getStreet(): List<Street>
 
 
-    @GET("houses")//https://dummyjson.com
+    @GET("houses")
     suspend fun getHouse(@Query("street_id") street_id:String): List<House>
 
 
